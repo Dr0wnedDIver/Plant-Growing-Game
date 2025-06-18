11d4ce8e21ab0c79454cfdf2739595d6f6494f3f
Dr0wnedDIver <robinboe99@live.de>
2025-06-18 14:58:07 +0200

    Cleared up code and got character position related to game.

--- Scripts/Game_MAIN_loop.gd
+++ Scripts/Game_MAIN_loop.gd
@@ -3,12 +3,25 @@ extends Node2D
 #load the scene of Field
 var field_scene: PackedScene = load("res://Scenes/Field_Object.tscn")
 
+var newCharacterPosition: Vector2 
+var newCharacterDirection: Vector2 
 
+func _on_character_player_position(CharacterPosition: Variant):
+	newCharacterPosition = CharacterPosition
+	
+func _on_character_character_direction(CharacterDirection: Variant):
+	newCharacterDirection	 = CharacterDirection
 
+func _place_field():
+	if Input.is_action_just_pressed("interact"):
+		print(newCharacterDirection)
+		print(newCharacterPosition)
+		#create instance of field
+		var field = field_scene.instantiate()
+		#attach field instance to scene tree
+		$Fields.add_child(field)
+		field.show()
+		field.set_position(newCharacterPosition)
 
-#func _place_field(playerposition):
-	##create instance of field
-	#var field = field_scene.instantiate()
-	#
-	##attach field instance to scene tree
-	#$Fields.add_child(field)
+func _process(delta: float) -> void:
+	_place_field()
--- Scripts/field_object.gd
+++ Scripts/field_object.gd
@@ -4,31 +4,31 @@ extends StaticBody2D
 func _ready() -> void:
 	hide()
 
-var field_scene: PackedScene = load("res://Scenes/Field_Object.tscn")
-var newCharacterPosition: Vector2 
-var newCharacterDirection: Vector2 
-
-
-func _on_character_player_position(CharacterPosition: Variant):
-	newCharacterPosition = CharacterPosition
-
-func _on_character_character_direction(CharacterDirection: Variant):
-	newCharacterDirection = CharacterDirection
-
-func _place_Field(PositionOfCharacter, DirectonOfCharacter):
-	if Input.is_action_just_pressed("interact"):
-		var direction: Vector2 = DirectonOfCharacter
-		var fieldposition: Vector2 = PositionOfCharacter
-		print(direction)
-		#print("Position" + str(fieldposition))
-		var field = field_scene.instantiate()
-		add_child(field)
-		show()
-		field.set_position(fieldposition)
+#var field_scene: PackedScene = load("res://Scenes/Field_Object.tscn")
+#var newCharacterPosition: Vector2 
+#var newCharacterDirection: Vector2 
+#
+#
+#func _on_character_player_position(CharacterPosition: Variant):
+	#newCharacterPosition = CharacterPosition
+#
+#func _on_character_character_direction(CharacterDirection: Variant):
+	#newCharacterDirection = CharacterDirection
+#
+#func _place_Field(PositionOfCharacter, DirectonOfCharacter):
+	#if Input.is_action_just_pressed("interact"):
+		#var direction: Vector2 = DirectonOfCharacter
+		#var fieldposition: Vector2 = PositionOfCharacter
+		#print(direction)
+		##print("Position" + str(fieldposition))
+		#var field = field_scene.instantiate()
+		#add_child(field)
+		#show()
+		#field.set_position(fieldposition)
 		
 		
 		
 
 func _process(delta: float):
-	_place_Field(newCharacterPosition, newCharacterDirection)
-	
+	#_place_Field(newCharacterPosition, newCharacterDirection)
+	pass
